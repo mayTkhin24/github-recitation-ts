e8533ae93f69facdea1dd279597f36e2a583e261
Ghalya AL-Eshaq <ghalyaal-eshaq@Ghalyas-MacBook-Pro.local>
2024-09-01 00:04:24 +0300

    fixing error Type string trivially inferred from a string literal, remove type annotatio

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,19 +1,18 @@
 // Endpoint for querying the fibonacci numbers
 
-import express from 'express';
 import { Request, Response } from 'express';
 import fibonacci from './fib';  // Import the Fibonacci function
 
 export default (req: Request, res: Response) => {
   // Safely get the number parameter from request
-  const numAsString: string = req.params.num;  
+  const numAsString = req.params.num;  // Type is inferred as `string`
   const num: number = parseInt(numAsString, 10);
 
   // Call the Fibonacci function
   const fibN: number = fibonacci(num);
 
   // Determine the result message
-  let result: string = `fibonacci(${num}) is ${fibN}`;
+  let result = `fibonacci(${num}) is ${fibN}`;
 
   if (fibN < 0) {
     result = `fibonacci(${num}) is undefined`;
